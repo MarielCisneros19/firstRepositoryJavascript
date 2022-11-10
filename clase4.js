c1a308862b56f2a68f8a8f91c56fad0cd93796ab
MarielCisneros19 <marucisneros1928@gmail.com>
2022-11-10 17:36:31 -0300

    first change

---
+++ clase4.js
@@ -0,0 +1,52 @@
+// 1.
+// function dominio(string){
+//     return("http://www."+string)
+// }
+
+// console.log(dominio("mercadolobre.com.ar"))
+
+//2.
+// let texto = ("string")
+//  console.log(texto.length)
+
+//3.
+// let dia = "lunes"
+
+// switch(dia){
+//     case "viernes":
+//         console.log("buen finde")
+//     break;
+
+//     case "lunes":
+//         console.log("empieza la semana")
+//      break;
+
+//      default: 
+//      console.log("buen día")
+//      break;
+// }
+// console.log(dia)
+
+//4.
+// let bicicletaA = {
+//     rodado: 18,
+//     marca: "Mountain Bike"
+// }
+
+// let bicicletaB = {
+//     rodado: 24,
+//     marca: "Aurora"
+// }
+
+// let bicicletaConRodado = bicicletaA.rodado > bicicletaB.rodado ? bicicletaA : bicicletaB;
+
+// console.log(bicicletaConRodado.marca)
+
+//5.
+// function loro(texto){
+// for (let i=1 ; i<=5 ; i++){
+//     console.log(texto)
+// }
+// }
+// console.log(loro("lucas"))
+
